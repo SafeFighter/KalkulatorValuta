8f12cc746e09ed080b2ce6e19b63187243541fc4
matija <matoma002@gmail.com>
2024-08-22 01:22:46 +0200

    Ucitavanje podataka baze u combo boxove

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KalkulatorValuta
 {
-    partial class Form1
+    partial class KalkulatorValuta
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(238, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(128, 99);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(318, 99);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 24);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 49);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(318, 49);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // KalkulatorValuta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(707, 340);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "KalkulatorValuta";
+            this.Text = "Kalkulator Valuta";
+            this.Load += new System.EventHandler(this.KalkulatorValuta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,36 @@ using System.Windows.Forms;
 
 namespace KalkulatorValuta
 {
-    public partial class Form1 : Form
+    public partial class KalkulatorValuta : Form
     {
-        public Form1()
+        string ConnString = "Data Source=MASTER2\\SQLEXPRESS;Initial Catalog=ValuteDB;Integrated Security=True;Encrypt=False;";
+        public KalkulatorValuta()
         {
             InitializeComponent();
+        }
+
+        private void KalkulatorValuta_Load(object sender, EventArgs e)
+        {
+            string upit = "SELECT Name from Currencies";
+            SqlConnection conn = new SqlConnection(ConnString);
+            conn.Open();
+
+            SqlCommand command = new SqlCommand(upit, conn);
+            SqlDataReader reader = command.ExecuteReader();
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+
+            while (reader.Read())
+            {
+                comboBox1.Items.Add(reader["Name"].ToString());
+                comboBox2.Items.Add(reader["Name"].ToString());
+            }
+
+
+            
+
+            conn.Close();
+
         }
     }
 }
